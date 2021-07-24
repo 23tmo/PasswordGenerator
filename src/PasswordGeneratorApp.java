e6cdbec3492605af1236d580299356ff96ec6652
23tmolano <23tmolano@castilleja.org>
2021-07-24 13:44:16 -0700

    finished second cake

--- src/PasswordGeneratorApp.java
+++ src/PasswordGeneratorApp.java
@@ -10,11 +10,15 @@ public class PasswordGeneratorApp extends PApplet{
     private static PasswordGeneratorApp app;
     final String INIT_MSG="Type length of password";
     String msg=INIT_MSG; // input message from user
-    private int length = 12;
+    private int length = 20;
     private String currentPassword = "";
+    private int counter = 0;
+    PFont mukta;
     PFont mono;
     PImage copy;
     PImage generate;
+    PImage copied;
+    PImage copyImg;
 
 
 
@@ -30,54 +34,63 @@ public class PasswordGeneratorApp extends PApplet{
 
     public void setup(){
         fill(0);
-        mono = createFont("MuktaMahee-ExtraBold", 32);
+        mukta = createFont("MuktaMahee-ExtraBold", 32);
+        mono = createFont("RobotoMono-Bold", 20);
         copy = loadImage("copy.png");
         generate = loadImage("generate.png");
+        copied = loadImage("copied.png");
+        copyImg = copy;
     }
 
     public void draw(){
         Password password = new Password(length, true, true, true, true);
         background(28,28,28);
-        textFont(mono);
+        textFont(mukta);
         int x = 2;
         int y = x+1;
-
-        // generate button
-        fill(255);
-        strokeWeight(5);
-        fill(28,28,28);
-        rect(200, 300, 150, 60);
-        fill(255);
-        textSize(12);
-        textAlign(CENTER, CENTER);
-        text("generate password", 275, 325);
 
         // user input
         fill(255);
         text(msg, width/2, height/2 - 100);
 
         // password
-        fill(255, 0, 0);
+        textFont(mono);
+        fill(230,234,81); //yellow
         textSize(20);
-        text(currentPassword, width/2, height/2);
+        textAlign(LEFT);
+        text(currentPassword, 50, height/2+30);
 
         // title yellow
         fill(230,234,81); //yellow
         textSize(65);
         textAlign(CENTER);
-        text("PASSWORD   GENERATOR.", width/2, 80);
+        text("PASSWORD GENERATOR.", width/2, 80);
 
         // title pink
         fill(216,89,244); //pink
-        text("PASSWORD   GENERATOR.", width/2-x, 80-y);
+        text("PASSWORD GENERATOR.", width/2-x, 80-y);
 
         // title blue
         fill(69,171,225); //blue
-        text("PASSWORD   GENERATOR.", width/2-2*x, 80-2*y);
+        text("PASSWORD GENERATOR.", width/2-2*x, 80-2*y);
 
-        //copy/generate cake
-        copy.resize(0, 200);
-        image(copy, 825, height/2-125);
+        // generate cake
+        generate.resize(0, 170);
+        image(generate, 650, height/2-101);
+        textFont(mono);
+        fill(69,171,225);
+        text("generate", 730, height/2);
+        // w:170  h: 170
+        // x: 650 y: height/2-101
+
+        //copy cake
+        copyImg.resize(0, 200);
+        image(copyImg, 825, height/2-132);
+        textFont(mono);
+        fill(69,171,225);
+        text("copy", 940, height/2-90);
+        //w: 220 h:200
+        //x: 825 y: height/2 - 125
 
     }
     public void mouseClicked() {
@@ -85,17 +98,34 @@ public class PasswordGeneratorApp extends PApplet{
         msg=INIT_MSG;  //Reset message, prepare for a new message
     }
 
-    public void handleMouseClicked(int x, int y){
-        if (x > 200 && x < (200 + 150) &&
-                y > 300 && y < (300 + 60)){
-            currentPassword = SettingMethods.generatePassword();
+    public void copyOrCopied(){
+        counter++;
+        if(counter % 2 == 0){
+            copyImg = copied;
+        }
+        else{
+            copyImg = copy;
         }
 
+    }
+    public void handleMouseClicked(int x, int y){
         //copy cake
         if (x > 825 && x < (825 + 220) &&
                 y > height/2-125 && y < (height/2-125 + 200)){
             copyToClipboard(currentPassword);
+            copyOrCopied();
         }
+
+        // generate cake
+        if (x > 650 && x < 650 + 170 &&
+                y > height/2-101 && y < (height/2-101 + 170)){
+            currentPassword = SettingMethods.generatePassword();
+        }
+
+        //if (x > XPOS && x < (XPOS + WIDTH) &&
+        //    y > YPOS && y < (YPOS + HEIGHT)){
+        //    copyToClipboard(currentPassword);
+        //}
     }
 
     public void keyPressed() {
